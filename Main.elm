2b69e73b7a024d245d6aca79e59d9443b987ab68
Dave Compton <davecompton7@gmail.com>
2017-12-05 02:09:34 +0000

    adding oauth features

--- Main.elm
+++ Main.elm
@@ -1,9 +1,17 @@
-import Html exposing (Html, text, program, button)
+import Html exposing (Html, text, program, button, a, div)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
 
 type alias Model = Int
 
 type Msg = NoOp
+
+clientId = "8256469ec6a458a2b111"
+redirectUri = "https://dc25.github.io/oauthElm"
+scope = "repo:user"
+state = "w9erwlksjdf;kajdsf"
+
+githubOauthUri = "https://github.com/login/oauth/authorize?client_id=" ++ clientId ++ "&redirect_uri=" ++ redirectUri ++ "&scope=" ++ scope ++ "&state=" ++ state
 
 init : (Model, Cmd Msg)
 init = (0, Cmd.none)
@@ -12,7 +20,10 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update c m = (m+1, Cmd.none)
 
 view : Model -> Html Msg
-view m = button [onClick NoOp] [text (toString m)]
+view m = div []
+             [ a [href githubOauthUri] [text "github auth"]
+             , button [onClick NoOp] [text (toString m)]
+             ]
 
 main : Program Never Model Msg
 main =
