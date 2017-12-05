cbdafb61542379cbf397a186abde18e2cbfcb61d
Dave Compton <davecompton7@gmail.com>
2017-12-05 10:50:43 -0800

    added url parsing

--- Main.elm
+++ Main.elm
@@ -4,7 +4,12 @@ import Html.Attributes exposing (href)
 import Navigation exposing (..)
 import UrlParser as Url exposing (..)
 
-type alias Model = Int
+type alias Oauth = {code: String, state: String}
+
+type alias Model = 
+    { value:Int
+    , oauth: Maybe Oauth
+    }
 
 type Msg =   NoOp
            | UrlChange Navigation.Location
@@ -14,31 +19,46 @@ redirectUri = "https://dc25.github.io/oauthElm"
 scope = "repo:user"
 state = "w9erwlksjdf;kajdsf"
 
-githubOauthUri = "https://github.com/login/oauth/authorize?client_id=" ++ clientId ++ "&redirect_uri=" ++ redirectUri ++ "&scope=" ++ scope ++ "&state=" ++ state
+githubOauthUri = "https://github.com/login/oauth/authorize"
+                     ++ "?client_id=" ++ clientId 
+                     ++ "&redirect_uri=" ++ redirectUri 
+                     ++ "&scope=" ++ scope 
+                     ++ "&state=" ++ state
 
-type Route = OauthCode (Maybe String) | OauthState Int
+type Route = OauthCode (Maybe String) (Maybe String)
 route : Parser (Route -> a) a
-route =
-  oneOf
-    [ map OauthCode (s "blog" <?> stringParam "search")
-    , map OauthState (s "blog" </> int)
-    ]
+route = map OauthCode (s "blog" <?> stringParam "code" <?> stringParam "string")
 
 initModel : Maybe Route -> ( Model, Cmd Msg )
-initModel r = ( 0, Cmd.none )
+initModel r = ( {value=0, oauth=Nothing}, Cmd.none )
 
 init : Navigation.Location -> ( Model, Cmd Msg )
 init location =
     initModel (Url.parseHash route location)
 
+updateOauth : Model -> Navigation.Location -> (Model, Cmd Msg)
+updateOauth model location =
+    let oauth = parsePath route location
+    in case oauth of
+        Just (OauthCode (Just c) (Just s)) -> ({model | oauth = Just {code=c,state=s}}, Cmd.none)
+        _ -> (model, Cmd.none)
+
+
 update : Msg -> Model -> (Model, Cmd Msg)
-update c m = (m+1, Cmd.none)
+update c m = 
+    case c of
+        UrlChange location -> updateOauth m location
+        NoOp -> ({m | value=m.value+1}, Cmd.none)
 
 view : Model -> Html Msg
 view m = div []
-             [ a [href githubOauthUri] [text "github auth"]
-             , button [onClick NoOp] [text (toString m)]
-             ]
+             (  [ a [href githubOauthUri] [text "github auth"]
+                , button [onClick NoOp] [text (toString m)]
+                ] 
+             ++ [
+                   text "hello"
+                ]
+             )
 
 main : Program Never Model Msg
 main =
